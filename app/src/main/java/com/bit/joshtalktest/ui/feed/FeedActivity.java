c05b8c0910fc7fc680a8d3efab34aaae1037c3a0
Mohit Kumar Saini <mca.mohitkumar@gmail.com>
2019-06-28 21:03:08 +0530

    Add feed activity

--- app/src/main/java/com/bit/joshtalktest/ui/feed/FeedActivity.java
+++ app/src/main/java/com/bit/joshtalktest/ui/feed/FeedActivity.java
@@ -1,13 +1,61 @@
 package com.bit.joshtalktest.ui.feed;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProvider.AndroidViewModelFactory;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
 
-public class FeedActivity extends AppCompatActivity {
+import com.bit.joshtalktest.R;
+
+public class FeedActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private FeedViewModel feedViewModel;
+    private RecyclerView feedRecyclerView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        feedViewModel = new ViewModelProvider(this, new AndroidViewModelFactory(getApplication())).get(FeedViewModel.class);
+        initView();
+        addObserver();
+        initDownload();
+
+    }
+
+
+    private void addObserver() {
+
+    }
+
+    private void initView() {
+        feedRecyclerView=findViewById(R.id.feed_recycle_view);
+        feedRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mSwipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_blue_dark);
+
+
+    }
+
+    private void initDownload() {
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(true);
+            }
+        });
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }
---
+++ app/src/main/java/com/bit/joshtalktest/ui/feed/FeedViewModel.java
@@ -0,0 +1,13 @@
+package com.bit.joshtalktest.ui.feed;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+public class FeedViewModel extends AndroidViewModel {
+
+    public FeedViewModel(@NonNull final Application application) {
+        super(application);
+    }
+}
